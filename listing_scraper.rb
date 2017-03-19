e77803314aebe0ef46f2caadef66d8c3559bb2bc
Daniel Welsh <email": null>
2017-03-19 14:27:54 -0700

    Refactor formatting methods into seperate module.

--- listing_scraper.rb
+++ listing_scraper.rb
@@ -1,11 +1,13 @@
 require_relative 'house'
 require 'nokogiri'
 require 'open-uri'
+require_relative 'formating_module'
 
 
 # https://www.remax.ca/ab/edmonton-real-estate/na-1902-9923-103-street-na-wp_id167819998-lst/
 class ListingScraper
   attr_reader :args
+  include ScrapingFormatting
 
   def initialize(link)
     @args = {}
@@ -163,32 +165,6 @@ class ListingScraper
       rows_info << details
     end
     @args[:room_details] = rows_info
-  end
-
-
-private
-  def remove_colon(string)
-    if string.include?(':')
-      match = string.match(/(.*)([:])/)
-      match[1]
-    else
-      string
-    end
-  end
-
-  def format_sym(string)
-    string = remove_colon(string)
-    string = string.downcase.gsub(' ', '_')
-  end
-
-  def yes_to_boolean(string)
-    if string == 'yes'
-      true
-    elsif string == 'no'
-      false
-    else
-      string
-    end
   end
 
 end
