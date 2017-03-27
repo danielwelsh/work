89e4c85336ad925500325c7c20ef91006108b6c9
Daniel Welsh <danielwelsh@borealproductions.com>
2017-03-26 18:21:42 -0700

    Add create_tables migration

--- db/migrate/20170326174313_create_base_schema.rb
+++ db/migrate/20170326174313_create_base_schema.rb
@@ -5,9 +5,75 @@ class CreateBaseSchema < ActiveRecord::Migration
       t.string :room_type
       t.string :dimension
 
-      t.timestamps
+      t.timestamps(null: false)
     end
 
-    create_table :
+    create_table :houses do |t|
+      t.string :mls_listing_id
+      t.integer :property_price
+      t.string :street
+      t.status :status
+      t.integer :bedrooms
+      t.integer :bathrooms
+      t.integer :sq_feet
+      t.integer :days_on_market
+      t.string :amenities
+      t.boolean :balcony
+      t.integer :age_of_building
+      t.string :parking_type
+      t.string :heating
+      t.string :basement
+      t.string :exterior
+      t.string :flooring
+      t.string :roofing
+      t.string :property_description
+      t.integer :agent_id
+      t.integer :community_id
+      t.integer :building_type_id
+      t.integer :province_id
+      t.integer :city_id
+      t.integer :postal_code_id
+
+      t.timestamps(null: false)
+
+    end
+
+    create_table :provinces do |t|
+      t.string :name
+
+      t.timestamps(null: false)
+    end
+
+    create_table :cities do |t|
+      t.string :name
+
+      t.timestamps(null: false)
+    end
+
+    create_table :postal_codes do |t|
+      t.string :code
+
+      t.timestamps(null: false)
+    end
+
+    create_table :agents do |t|
+      t.string :name
+
+      t.timestamps(null: false)
+    end
+
+    create_table :communities do |t|
+      t.string :name
+
+      t.timestamps(null: false)
+    end
+
+    create_table :building_types do |t|
+      t.string :name
+
+      t.timestamps(null: false)
+    end
+
+
   end
 end
